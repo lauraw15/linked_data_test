35ad84113c16cb77e42d94a3ddc52ec2f474e2b7
Laura Wilsey <lauraw15@DN0a232410.SUNet>
2016-06-07 14:30:35 -0700

    updating performance group and group_spec files

--- spec/lib/performance_group_spec.rb
+++ spec/lib/performance_group_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper.rb'
 require 'PerformanceGroup'
+
 describe 'PerformanceGroup' do
   it 'can instantiate' do
     foo=PerformanceGroup.new
     expect(foo).to be_kind_of PerformanceGroup
+  end
+  
+  it 'has a name' do
+    foo = PerformanceGroup.new
+    foo.name = 'The Beatles'
+    expect(foo.name).to eq 'The Beatles'
   end
 end
