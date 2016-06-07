25a9137879b155b80e9bc35ef77fcf9f044daf79
Laura Wilsey <lauraw15@DN0a232410.SUNet>
2016-06-07 13:42:17 -0700

    adding tests

---
+++ spec/lib/performance_group_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper.rb'
+require 'PerformanceGroup'
+describe 'PerformanceGroup' do
+  it 'can instantiate' do
+    foo=PerformanceGroup.new
+    expect (foo).to be_kind_of PerformanceGroup
+  end
+end
